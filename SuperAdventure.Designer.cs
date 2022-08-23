5393f6555fbb73ebcdb2854c3e200d5537bbc5fc
patrickrweber <patrickrweber@hotmail.com>
2022-08-23 20:13:32 -0300

    UI

--- SuperAdventure.Designer.cs
+++ SuperAdventure.Designer.cs
@@ -41,12 +41,27 @@ namespace SuperAdventure
             this.lblInputName = new System.Windows.Forms.Label();
             this.txtName = new System.Windows.Forms.TextBox();
             this.btnConfirmar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboWeapons = new System.Windows.Forms.ComboBox();
+            this.cboPotions = new System.Windows.Forms.ComboBox();
+            this.btnUseWeapon = new System.Windows.Forms.Button();
+            this.btnUsePotion = new System.Windows.Forms.Button();
+            this.btnNorth = new System.Windows.Forms.Button();
+            this.btnEast = new System.Windows.Forms.Button();
+            this.btnSouth = new System.Windows.Forms.Button();
+            this.btnWest = new System.Windows.Forms.Button();
+            this.rtbLocation = new System.Windows.Forms.RichTextBox();
+            this.rtbMessages = new System.Windows.Forms.RichTextBox();
+            this.dgvInventory = new System.Windows.Forms.DataGridView();
+            this.dgvQuests = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQuests)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Location = new System.Drawing.Point(16, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(30, 15);
             this.label1.TabIndex = 0;
@@ -55,7 +70,7 @@ namespace SuperAdventure
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Location = new System.Drawing.Point(16, 60);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(34, 15);
             this.label2.TabIndex = 1;
@@ -64,7 +79,7 @@ namespace SuperAdventure
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 88);
+            this.label3.Location = new System.Drawing.Point(16, 88);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(67, 15);
             this.label3.TabIndex = 2;
@@ -73,7 +88,7 @@ namespace SuperAdventure
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 114);
+            this.label4.Location = new System.Drawing.Point(16, 114);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 15);
             this.label4.TabIndex = 3;
@@ -149,11 +164,159 @@ namespace SuperAdventure
             this.btnConfirmar.UseVisualStyleBackColor = true;
             this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(617, 531);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Selecionar Ação";
+            // 
+            // cboWeapons
+            // 
+            this.cboWeapons.FormattingEnabled = true;
+            this.cboWeapons.Location = new System.Drawing.Point(369, 559);
+            this.cboWeapons.Name = "cboWeapons";
+            this.cboWeapons.Size = new System.Drawing.Size(121, 23);
+            this.cboWeapons.TabIndex = 13;
+            // 
+            // cboPotions
+            // 
+            this.cboPotions.FormattingEnabled = true;
+            this.cboPotions.Location = new System.Drawing.Point(369, 593);
+            this.cboPotions.Name = "cboPotions";
+            this.cboPotions.Size = new System.Drawing.Size(121, 23);
+            this.cboPotions.TabIndex = 14;
+            // 
+            // btnUseWeapon
+            // 
+            this.btnUseWeapon.Location = new System.Drawing.Point(620, 559);
+            this.btnUseWeapon.Name = "btnUseWeapon";
+            this.btnUseWeapon.Size = new System.Drawing.Size(75, 23);
+            this.btnUseWeapon.TabIndex = 15;
+            this.btnUseWeapon.Text = "Usar";
+            this.btnUseWeapon.UseVisualStyleBackColor = true;
+            this.btnUseWeapon.Click += new System.EventHandler(this.btnUseWeapon_Click);
+            // 
+            // btnUsePotion
+            // 
+            this.btnUsePotion.Location = new System.Drawing.Point(620, 593);
+            this.btnUsePotion.Name = "btnUsePotion";
+            this.btnUsePotion.Size = new System.Drawing.Size(75, 23);
+            this.btnUsePotion.TabIndex = 16;
+            this.btnUsePotion.Text = "Usar";
+            this.btnUsePotion.UseVisualStyleBackColor = true;
+            this.btnUsePotion.Click += new System.EventHandler(this.btnUsePotion_Click);
+            // 
+            // btnNorth
+            // 
+            this.btnNorth.Location = new System.Drawing.Point(493, 433);
+            this.btnNorth.Name = "btnNorth";
+            this.btnNorth.Size = new System.Drawing.Size(75, 23);
+            this.btnNorth.TabIndex = 17;
+            this.btnNorth.Text = "Norte";
+            this.btnNorth.UseVisualStyleBackColor = true;
+            this.btnNorth.Click += new System.EventHandler(this.btnNorth_Click);
+            // 
+            // btnEast
+            // 
+            this.btnEast.Location = new System.Drawing.Point(573, 457);
+            this.btnEast.Name = "btnEast";
+            this.btnEast.Size = new System.Drawing.Size(75, 23);
+            this.btnEast.TabIndex = 18;
+            this.btnEast.Text = "Leste";
+            this.btnEast.UseVisualStyleBackColor = true;
+            this.btnEast.Click += new System.EventHandler(this.btnEast_Click);
+            // 
+            // btnSouth
+            // 
+            this.btnSouth.Location = new System.Drawing.Point(493, 487);
+            this.btnSouth.Name = "btnSouth";
+            this.btnSouth.Size = new System.Drawing.Size(75, 23);
+            this.btnSouth.TabIndex = 19;
+            this.btnSouth.Text = "Sul";
+            this.btnSouth.UseVisualStyleBackColor = true;
+            this.btnSouth.Click += new System.EventHandler(this.btnSouth_Click);
+            // 
+            // btnWest
+            // 
+            this.btnWest.Location = new System.Drawing.Point(412, 457);
+            this.btnWest.Name = "btnWest";
+            this.btnWest.Size = new System.Drawing.Size(75, 23);
+            this.btnWest.TabIndex = 20;
+            this.btnWest.Text = "Oeste";
+            this.btnWest.UseVisualStyleBackColor = true;
+            this.btnWest.Click += new System.EventHandler(this.btnWest_Click);
+            // 
+            // rtbLocation
+            // 
+            this.rtbLocation.Location = new System.Drawing.Point(347, 19);
+            this.rtbLocation.Name = "rtbLocation";
+            this.rtbLocation.ReadOnly = true;
+            this.rtbLocation.Size = new System.Drawing.Size(360, 105);
+            this.rtbLocation.TabIndex = 21;
+            this.rtbLocation.Text = "";
+            // 
+            // rtbMessages
+            // 
+            this.rtbMessages.Location = new System.Drawing.Point(347, 130);
+            this.rtbMessages.Name = "rtbMessages";
+            this.rtbMessages.ReadOnly = true;
+            this.rtbMessages.Size = new System.Drawing.Size(360, 286);
+            this.rtbMessages.TabIndex = 22;
+            this.rtbMessages.Text = "";
+            // 
+            // dgvInventory
+            // 
+            this.dgvInventory.AllowUserToAddRows = false;
+            this.dgvInventory.AllowUserToDeleteRows = false;
+            this.dgvInventory.AllowUserToResizeColumns = false;
+            this.dgvInventory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInventory.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvInventory.Enabled = false;
+            this.dgvInventory.Location = new System.Drawing.Point(16, 130);
+            this.dgvInventory.MultiSelect = false;
+            this.dgvInventory.Name = "dgvInventory";
+            this.dgvInventory.RowHeadersVisible = false;
+            this.dgvInventory.RowTemplate.Height = 25;
+            this.dgvInventory.Size = new System.Drawing.Size(312, 309);
+            this.dgvInventory.TabIndex = 23;
+            // 
+            // dgvQuests
+            // 
+            this.dgvQuests.AllowUserToAddRows = false;
+            this.dgvQuests.AllowUserToDeleteRows = false;
+            this.dgvQuests.AllowUserToResizeColumns = false;
+            this.dgvQuests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvQuests.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvQuests.Enabled = false;
+            this.dgvQuests.Location = new System.Drawing.Point(16, 446);
+            this.dgvQuests.MultiSelect = false;
+            this.dgvQuests.Name = "dgvQuests";
+            this.dgvQuests.RowHeadersVisible = false;
+            this.dgvQuests.RowTemplate.Height = 25;
+            this.dgvQuests.Size = new System.Drawing.Size(312, 189);
+            this.dgvQuests.TabIndex = 24;
+            // 
             // SuperAdventure
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(719, 651);
+            this.Controls.Add(this.dgvQuests);
+            this.Controls.Add(this.dgvInventory);
+            this.Controls.Add(this.rtbMessages);
+            this.Controls.Add(this.rtbLocation);
+            this.Controls.Add(this.btnWest);
+            this.Controls.Add(this.btnSouth);
+            this.Controls.Add(this.btnEast);
+            this.Controls.Add(this.btnNorth);
+            this.Controls.Add(this.btnUsePotion);
+            this.Controls.Add(this.btnUseWeapon);
+            this.Controls.Add(this.cboPotions);
+            this.Controls.Add(this.cboWeapons);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.btnConfirmar);
             this.Controls.Add(this.txtName);
             this.Controls.Add(this.lblInputName);
@@ -167,7 +330,9 @@ namespace SuperAdventure
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "SuperAdventure";
-            this.Text = "MyGame";
+            this.Text = "Super Adventure";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInventory)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvQuests)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -187,6 +352,19 @@ namespace SuperAdventure
         private System.Windows.Forms.Label lblInputName;
         private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboWeapons;
+        private System.Windows.Forms.ComboBox cboPotions;
+        private System.Windows.Forms.Button btnUseWeapon;
+        private System.Windows.Forms.Button btnUsePotion;
+        private System.Windows.Forms.Button btnNorth;
+        private System.Windows.Forms.Button btnEast;
+        private System.Windows.Forms.Button btnSouth;
+        private System.Windows.Forms.Button btnWest;
+        private System.Windows.Forms.RichTextBox rtbLocation;
+        private System.Windows.Forms.RichTextBox rtbMessages;
+        private System.Windows.Forms.DataGridView dgvInventory;
+        private System.Windows.Forms.DataGridView dgvQuests;
     }
 }
 
--- SuperAdventure.cs
+++ SuperAdventure.cs
@@ -13,32 +13,364 @@ namespace SuperAdventure
 {
     public partial class SuperAdventure : Form
     {
-        private Player player;
-        private Location location;
+        private Player _player;
+        private Monster _currentMonster;
         public SuperAdventure()
         {
             InitializeComponent();
 
-            location = new Location(1, "Casa", "Essa é a sua casa");
+            _player = new Player(10, 10, 20, 0, 1);
+            MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
+            _player.Inventory.Add(new InventoryItem(
+            World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
 
-            player = new Player(null, 10, 0, 1, 10, 10);
-
-            lblHitPoints.Text = player.CurrentHitPoints.ToString();
-            lblGold.Text = player.Gold.ToString();
-            lblExperience.Text = player.ExperiencePoints.ToString();
-            lblLevel.Text = player.Level.ToString();
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
 
         }
 
         private void btnConfirmar_Click(object sender, EventArgs e)
         {
-            player.Name = txtName.Text;
-            lblNome.Text = player.Name;
+            _player.Name = txtName.Text;
+            lblNome.Text = _player.Name;
 
             lblNome.Visible = true;
             btnConfirmar.Visible = false;
             lblInputName.Visible = false;
             txtName.Visible = false;
+        }
+
+        private void btnWest_Click(object sender, EventArgs e)
+        {
+            MoveTo(_player.CurrentLocation.LocationToWest);
+        }
+
+        private void btnNorth_Click(object sender, EventArgs e)
+        {
+            MoveTo(_player.CurrentLocation.LocationToNorth);
+        }
+
+        private void btnEast_Click(object sender, EventArgs e)
+        {
+            MoveTo(_player.CurrentLocation.LocationToEast);
+        }
+
+        private void btnSouth_Click(object sender, EventArgs e)
+        {
+            MoveTo(_player.CurrentLocation.LocationToSouth);
+        }
+
+        private void MoveTo(Location newLocation)
+        {
+            //Does the location have any required items
+            if (newLocation.ItemRequiredToEnter != null)
+            {
+                bool playerHasRequiredItem = false;
+                foreach (InventoryItem ii in _player.Inventory)
+                {
+                    if (ii.Details.Id == newLocation.ItemRequiredToEnter.Id)
+                    {
+                        playerHasRequiredItem = true;
+                        break;
+                    }
+                }
+                if (!playerHasRequiredItem)
+                {
+                    rtbMessages.Text += "Você precisa ter um  " + newLocation.ItemRequiredToEnter.Name +
+                        " para entrar nessa localização." + Environment.NewLine;
+                    return;
+                }
+            }
+
+            _player.CurrentLocation = newLocation;
+
+            btnNorth.Visible = (newLocation.LocationToNorth != null);
+            btnEast.Visible = (newLocation.LocationToEast != null);
+            btnSouth.Visible = (newLocation.LocationToSouth != null);
+            btnWest.Visible = (newLocation.LocationToWest != null);
+
+            rtbLocation.Text = newLocation.Name + Environment.NewLine;
+            rtbLocation.Text += newLocation.Description + Environment.NewLine;
+
+            _player.CurrentHitPoints = _player.MaximumHitPoints;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+
+            if (newLocation.QuestAvailableHere != null)
+            {
+                bool playerAlreadyHasQuest = false;
+                bool playerAlreadyCompletedQuest = false;
+
+                foreach (PlayerQuest playerQuest in _player.Quests)
+                {
+                    if (playerQuest.Details.Id == newLocation.QuestAvailableHere.Id)
+                    {
+                        playerAlreadyHasQuest = true;
+
+                        if (playerQuest.IsCompleted)
+                        {
+                            playerAlreadyCompletedQuest = true;
+                        }
+                    }
+                }
+
+                if (playerAlreadyHasQuest)
+                {
+                    if (!playerAlreadyCompletedQuest)
+                    {
+                        bool playerHasAllItemsToCompleteQuest = true;
+
+                        foreach (QuestCompletionItem qci in
+                        newLocation.QuestAvailableHere.QuestCompletionItems)
+                        {
+                            bool foundItemInPlayersInventory = false;
+
+                            foreach (InventoryItem ii in _player.Inventory)
+                            {
+                                if (ii.Details.Id == qci.Details.Id)
+                                {
+                                    foundItemInPlayersInventory = true;
+                                    if (ii.Quantity < qci.Quantity)
+                                    {
+                                        playerHasAllItemsToCompleteQuest = false;
+                                        break;
+                                    }
+                                    break;
+                                }
+                            }
+                            
+                            if (!foundItemInPlayersInventory)
+                            { 
+                                playerHasAllItemsToCompleteQuest = false; 
+                                break;
+                            }
+                        }
+                        if (playerHasAllItemsToCompleteQuest)
+                        {
+                            rtbMessages.Text += Environment.NewLine;
+                            rtbMessages.Text += "Você completou a  " +
+                                newLocation.QuestAvailableHere.Name +
+                                " missão." + Environment.NewLine;
+                        
+                            foreach (QuestCompletionItem qci in
+                                newLocation.QuestAvailableHere.QuestCompletionItems)
+                            {
+                                foreach (InventoryItem ii in _player.Inventory)
+                                {
+                                    if (ii.Details.Id == qci.Details.Id)
+                                    {
+                                        ii.Quantity -= qci.Quantity;
+                                        break;
+                                    }
+                                }
+                            }
+
+                            rtbMessages.Text += "Você recebeu: " + Environment.NewLine;
+                            rtbMessages.Text +=
+                                newLocation.QuestAvailableHere.RewardExperiencePoints.ToString() +
+                                    " Pontos de experiência." + Environment.NewLine;
+                            rtbMessages.Text +=
+                                newLocation.QuestAvailableHere.RewardGold.ToString() +
+                                    " ouro" + Environment.NewLine;
+                            rtbMessages.Text +=
+                                newLocation.QuestAvailableHere.RewardItem.Name +
+                                    Environment.NewLine;
+                            rtbMessages.Text += Environment.NewLine;
+
+                            _player.ExperiencePoints +=
+                                newLocation.QuestAvailableHere.RewardExperiencePoints;
+                            _player.Gold += newLocation.QuestAvailableHere.RewardGold;
+
+                            bool addedItemToPlayerInventory = false;
+
+                            foreach (InventoryItem ii in _player.Inventory)
+                            {
+                                if (ii.Details.Id ==
+                                    newLocation.QuestAvailableHere.RewardItem.Id)
+                                {
+                                    ii.Quantity++;
+                            
+                                    addedItemToPlayerInventory = true;
+                            
+                                    break;
+                                }
+                            }
+
+                            if (!addedItemToPlayerInventory)
+                            {
+                                _player.Inventory.Add(new InventoryItem(
+                                    newLocation.QuestAvailableHere.RewardItem, 1));
+                            }
+
+                            foreach (PlayerQuest pq in _player.Quests)
+                            {
+                                if (pq.Details.Id == newLocation.QuestAvailableHere.Id)
+                                {
+                                    pq.IsCompleted = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    rtbMessages.Text += "You receive the " +
+                        newLocation.QuestAvailableHere.Name +
+                            " quest." + Environment.NewLine;
+                    rtbMessages.Text += newLocation.QuestAvailableHere.Description +
+                        Environment.NewLine;
+                    rtbMessages.Text += "To complete it, return with:" +
+                        Environment.NewLine;
+
+                    foreach (QuestCompletionItem qci in
+                        newLocation.QuestAvailableHere.QuestCompletionItems)
+                    {
+                        if (qci.Quantity == 1)
+                        {
+                            rtbMessages.Text += qci.Quantity.ToString() + " " +
+                                qci.Details.Name + Environment.NewLine;
+                        }
+                        else
+                        {
+                            rtbMessages.Text += qci.Quantity.ToString() + " " +
+                                qci.Details.NamePlural + Environment.NewLine;
+                        }
+                    }
+                    rtbMessages.Text += Environment.NewLine;
+                    
+                    _player.Quests.Add(new PlayerQuest(newLocation.QuestAvailableHere));
+                }
+            }
+
+            if (newLocation.MonsterLivingHere != null)
+            {
+                rtbMessages.Text += "You see a " + newLocation.MonsterLivingHere.Name +
+                    Environment.NewLine;
+
+                Monster standardMonster = World.MonsterByID(
+                    newLocation.MonsterLivingHere.Id);
+                
+                _currentMonster = new Monster(standardMonster.Id, standardMonster.Name,
+                    standardMonster.MaximumDamage, standardMonster.RewardExperiencePoints,
+                        standardMonster.RewardGold, standardMonster.CurrentHitPoints,
+                            standardMonster.MaximumHitPoints);
+                
+                foreach (LootItem lootItem in standardMonster.LootTable)
+                {
+                    _currentMonster.LootTable.Add(lootItem);
+                }
+
+                cboWeapons.Visible = true;
+                cboPotions.Visible = true;
+                btnUseWeapon.Visible = true;
+                btnUsePotion.Visible = true;
+            }
+            else
+            {
+                _currentMonster = null;
+                
+                cboWeapons.Visible = false;
+                cboPotions.Visible = false;
+                btnUseWeapon.Visible = false;
+                btnUsePotion.Visible = false;
+            }
+
+            dgvInventory.RowHeadersVisible = false;
+           
+            dgvInventory.ColumnCount = 2;
+            dgvInventory.Columns[0].Name = "Name";
+            dgvInventory.Columns[0].Width = 197;
+            dgvInventory.Columns[1].Name = "Quantity";
+            
+            dgvInventory.Rows.Clear();
+            
+            foreach (InventoryItem inventoryItem in _player.Inventory)
+            {
+                if (inventoryItem.Quantity > 0)
+                {
+                    dgvInventory.Rows.Add(new[] { inventoryItem.Details.Name,
+                        inventoryItem.Quantity.ToString() });
+                }
+            }
+
+            dgvQuests.RowHeadersVisible = false;
+
+            dgvQuests.ColumnCount = 2;
+            dgvQuests.Columns[0].Name = "Name";
+            dgvQuests.Columns[0].Width = 197;
+            dgvQuests.Columns[1].Name = "Done?";
+
+            dgvQuests.Rows.Clear();
+
+            foreach (PlayerQuest playerQuest in _player.Quests)
+            {
+                dgvQuests.Rows.Add(new[] { playerQuest.Details.Name,
+                    playerQuest.IsCompleted.ToString() });
+            }
+
+            List<Weapon> weapons = new List<Weapon>();
+
+            foreach (InventoryItem inventoryItem in _player.Inventory)
+            {
+                if (inventoryItem.Details is Weapon)
+                {
+                    if (inventoryItem.Quantity > 0)
+                    {
+                        weapons.Add((Weapon)inventoryItem.Details);
+                    }
+                }
+            }
+            
+            if (weapons.Count == 0)
+            {
+                cboWeapons.Visible = false;
+                btnUseWeapon.Visible = false;
+            }
+            else
+            {
+                cboWeapons.DataSource = weapons;
+                cboWeapons.DisplayMember = "Name";
+                cboWeapons.ValueMember = "ID";
+                cboWeapons.SelectedIndex = 0;
+            }
+
+            List<HealingPotion> healingPotions = new List<HealingPotion>();
+            
+            foreach (InventoryItem inventoryItem in _player.Inventory)
+            {
+                if (inventoryItem.Details is HealingPotion)
+                {
+                    if (inventoryItem.Quantity > 0)
+                    {
+                        healingPotions.Add((HealingPotion)inventoryItem.Details);
+                    }
+                }
+            }
+            if (healingPotions.Count == 0)
+            {
+                cboPotions.Visible = false;
+                btnUsePotion.Visible = false;
+            }
+            else
+            {
+                cboPotions.DataSource = healingPotions;
+                cboPotions.DisplayMember = "Name";
+                cboPotions.ValueMember = "ID";
+            
+                cboPotions.SelectedIndex = 0;
+            }
+        }
+        private void btnUseWeapon_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUsePotion_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
